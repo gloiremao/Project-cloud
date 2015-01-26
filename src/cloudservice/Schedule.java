97332928d940dcabf1c4ae22f40ec7b01b74f3f1
gloiremao <gloiremao@gmail.com>
2015-01-26 21:43:28 +0800

    Cloud Project 20141206

---
+++ src/cloudservice/Schedule.java
@@ -0,0 +1,23 @@
+package cloudservice;
+
+public class Schedule {
+	public String time;
+	public boolean available;
+	
+	public Schedule(){
+		time = null;
+		available = false;
+	}
+	
+	public void setTime(String t){
+		this.time = t;
+	}
+	
+	public void setAvailable(int a){
+		if(a == 1){
+			this.available = true;
+		}else {
+			this.available = false;
+		}
+	}
+}
